4cc1236c8476234b10d668ed3edea2551aebc16b
Vinicios Neves <vinny@macbook.lan>
2022-04-16 22:54:40 +0100

    fim do desafio da aula 3

--- src/componentes/Rodape.tsx
+++ src/componentes/Rodape.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 
+import './Rodape.css'
+
 const Rodape = () => {
 
     const participantes = useListaDeParticipantes()
@@ -11,8 +13,9 @@ const Rodape = () => {
         navegarPara('/sorteio')
     }
 
-    return (<footer>
-        <button disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+    return (<footer className="rodape-configuracoes">
+        <button className="botao" disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+        <img src="/imagens/sacolas.png" alt="Sacolas de compras" />
     </footer>)
 }
 
