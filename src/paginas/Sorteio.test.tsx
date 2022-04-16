8c35e229a698fc28ec626536e8ff4248c423b6c4
Vinicios Neves <vinny@macbook.lan>
2022-04-17 00:57:30 +0100

    fim da aula 5

--- src/paginas/Sorteio.test.tsx
+++ src/paginas/Sorteio.test.tsx
@@ -38,7 +38,7 @@ describe('na pagina de sorteio', () => {
         </RecoilRoot>)
 
         const opcoes = screen.queryAllByRole('option')
-        expect(opcoes).toHaveLength(participantes.length)
+        expect(opcoes).toHaveLength(participantes.length + 1) // pq já vem uma option por padrão
     })
     test('o amigo secreto é exibido quando solicitado', () => {
         render(<RecoilRoot>
