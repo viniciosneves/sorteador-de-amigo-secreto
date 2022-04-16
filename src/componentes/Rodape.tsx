9be61989385befb88958a6459d8711156749ff4f
Vinicios Neves <vinny@macbook.lan>
2022-04-16 22:28:18 +0100

    fim da aula 3.4

---
+++ src/componentes/Rodape.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom"
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
+
+const Rodape = () => {
+
+    const participantes = useListaDeParticipantes()
+
+    const navegarPara = useNavigate()
+    
+    const iniciar = () => {
+        navegarPara('/sorteio')
+    }
+
+    return (<footer>
+        <button disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+    </footer>)
+}
+
+export default Rodape
